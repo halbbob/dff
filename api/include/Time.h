b84fc27785b10594e8b577edab2d218b98b5b7e3
rbe <rbe@digital-forensic.org>
2010-11-04 13:06:02 +0100

    * add file Time.cpp and Time.h I forgot in my previous commit.

---
+++ api/include/Time.h
@@ -0,0 +1,59 @@
+/*
+ * DFF -- An Open Source Digital Forensics Framework
+ * Copyright (C) 2009-2010 ArxSys
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ *
+ * See http://www.digital-forensic.org for more information about this
+ * project. Please do not directly contact any of the maintainers of
+ * DFF for assistance; the project provides a web site, mailing lists
+ * and IRC channels for your use.
+ *
+ * Author(s):
+ *  Romain Bertholon <rbe@digital-forensic.org>
+ *
+ */
+
+#ifndef __TIME_H__
+#define __TIME_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+
+#include "vtime.hpp"
+
+typedef struct	tm_s
+{
+  uint32_t	tm_year;
+  uint32_t	tm_mon;
+  uint32_t	tm_mday;
+  uint32_t	tm_hour;
+  uint32_t	tm_min;
+  uint32_t	tm_sec;
+}		tm_t;
+
+class	Time
+{
+public:
+  Time(uint64_t timestamp);
+  ~Time();
+
+  const tm_t *	tm() const;
+  vtime *	Vtime() const;
+
+private:
+  uint32_t	__february(uint32_t years);
+  uint32_t	__calc_year();
+  void		__convert();
+
+  uint32_t	__timestamp;
+  tm_t *	__tm;
+  vtime *	__vtime;
+  std::vector<std::pair<std::string, uint32_t> > __months_days;
+};
+
+#endif /* __TIME_H_  */
