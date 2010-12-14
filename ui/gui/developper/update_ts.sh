696a9f20b2e7f787a26d2dc5e32c05d764aa4617
cma <cma@digital-forensic.org>
2010-12-14 13:37:54 +0100

    Delete ui/gui/developper directory with two scripts in it. Those scripts run pylupdate4, lrelease and pyrcc4 but it have to be done by CMake. It is already the case for pyrcc4 in ui/gui/CMakeLists.txt, to be done for translations with pylupdate4 and lrelease binaries. Also remove no-more used *.ts files in ui/gui/i18n . This commit refs. #84.

--- ui/gui/developper/update_ts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pylupdate4 dff.py UI/*.py Browsers/*.py Tools/InfoVFS/*.py Tools/*.py Project/*.py Viewers/*.py Viewers/List/*.py -ts i18n/Dff_fr.ts
-
-pylupdate4 dff.py UI/*.py Browsers/*.py Tools/InfoVFS/*.py Tools/*.py Project/*.py Viewers/*.py Viewers/List/*.py -ts i18n/Dff_en.ts
-
-
-
