b8bfc9cb07ae371719784078fe4ebe2d5d223ab2
sja <sja@digital-forensic.org>
2011-03-27 01:05:17 +0000

    New module AFF AffNode Attributes as all info except for encryted blocks

---
+++ modules/connector/aff/aff.hpp
@@ -0,0 +1,67 @@
+/*
+ * DFF -- An Open Source Digital Forensics Framework
+ * Copyright (C) 2009-2011 ArxSys
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ *  
+ * See http://www.digital-forensic.org for more information about this
+ * project. Please do not directly contact any of the maintainers of
+ * DFF for assistance; the project provides a web site, mailing lists
+ * and IRC channels for your use.
+ * 
+ * Author(s):
+ *  Solal Jacob <sja@digital-forensic.org>
+ */
+
+#ifndef __AFF_HH__
+#define __AFF_HH__
+
+
+#include "fso.hpp"
+#include "node.hpp"
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <list>
+#include <vector>
+#include "variant.hpp"
+#include "vfs.hpp"
+#include "path.hpp"
+#include "affnode.hpp"
+
+class aff : public fso
+{
+private:
+  unsigned int	nfd;
+  std::string	basePath;
+  int		vread_error(int fd, void *buff, unsigned int size);
+  Node		*parent;
+  void		createTree(std::list<Variant *>);
+
+#ifndef WIN32
+  class ULocalNode*	__root; 
+#else
+  class WLocalNode*	__root;
+#endif
+
+public:
+#ifndef WIN32
+  void			iterdir(std::string path, Node* parent);
+#else
+  std::string		relativePath(std::string path);
+  void			createPath(std::string origPath);
+  void 			frec(const char *, Node *rfv);
+#endif
+  aff();
+  ~aff();
+  int32_t		vopen(Node* handle);
+  int32_t 		vread(int fd, void *buff, unsigned int size);
+  int32_t 		vclose(int fd);
+  uint64_t 		vseek(int fd, uint64_t offset, int whence);
+  int32_t		vwrite(int fd, void *buff, unsigned int size) { return 0; };
+  uint32_t		status(void);
+  uint64_t		vtell(int32_t fd);
+  virtual void	start(std::map<std::string, Variant* > args);
+};
+#endif
