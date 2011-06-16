559a56da72e3ca723dbc0df2371cc8e79dc6c7b6
sja <sja@digital-forensic.org>
2011-06-16 18:02:49 +0000

    Add needed method

--- api/apswvfs/apswvfs.py
+++ api/apswvfs/apswvfs.py
@@ -20,11 +20,51 @@ class apswVFile(apsw.VFSFile):
     print "flags " + str(flags)
     self.node = self.vfs.getnode(filename)
     self.vfile = self.node.open()
+    print "aspwVFile open FINISH"
 
   def xRead(self, size, offset):
+    print "APSW reading on the file"
     self.vfile.seek(offset)
     return self.vfile.read(size)
 
   def xWrite(self):
-    print "This file is read only"
+    print "APSW This file is read only"
     return 0
+
+  def xClose(self):
+    print "APSW closing file"
+    self.vfile.close()
+
+  def xSectorSize(self):
+    #hohoho pas tres forensic tous cam etonnant pour SQL 
+    print "APSW sector size"
+    return 512
+
+  def xDeviceCharacteristics(self):
+    print "APSW device characteristics"
+    #we have no caps
+    return 0 
+
+  def xLock(self, level):
+    print "APSW xlock "
+    #we are read only .... on lock by GIL 
+
+  def xUnlock(self, level):
+    print "APSW unlock"
+    #ask GIL what he think about that...
+
+  def xSync(self, flags):
+    print "APSW sync"
+    #read onlyyyyyyy
+
+  def xTruncate(self, newsize):
+    print "APSW truncate"
+    #we are reeeead only 
+  
+  def xFileSize(self):
+    print "APSW file size"
+    return self.vfile.node.size() 
+
+  def xFileControl(self, op, ptr):
+     print "APSW Xfile control WTF ? "
+     return False 
